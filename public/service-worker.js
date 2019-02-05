3d07cfaf749892a0fea3b4a76ad7ea735ce73014
Nathan Hipps <nathanhipps@gmail.com>
2019-02-05 16:16:46 -0500

    workbox settings

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,19 @@
-self.addEventListener('install', e => {
-  console.log('from my file', e)
-})
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+)
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+)
