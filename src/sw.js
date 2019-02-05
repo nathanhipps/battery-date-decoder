e418c6db5ad19ba4fc961259880ba7676635f842
Nathan Hipps <nathanhipps@gmail.com>
2019-02-05 15:05:11 -0500

    same

---
+++ src/sw.js
@@ -0,0 +1,7 @@
+// Cache the Google Fonts stylesheets with a stale while revalidate strategy.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  }),
+);
