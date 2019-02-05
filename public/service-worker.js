0843246c6c6509f0685600477273e02850c99969
Nathan Hipps <nathanhipps@gmail.com>
2019-02-05 16:26:59 -0500

    simple service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,39 @@
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.googleapis\.com/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'google-fonts-stylesheets',
-  })
-)
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches
+      .open('static')
+      .then(cache => {
+        cache.add('/img/icons/android-icon-36x36.png')
+        cache.add('/img/icons/android-icon-48x48.png')
+        cache.add('/img/icons/android-icon-72x72.png')
+        cache.add('/img/icons/android-icon-96x96.png')
+        cache.add('/img/icons/android-icon-144x144.png')
+        cache.add('/img/icons/android-icon-192x192.png')
+        cache.add('/img/icons/apple-icon-57x57.png')
+        cache.add('/img/icons/apple-icon-60x60.png')
+        cache.add('/img/icons/apple-icon-72x72.png')
+        cache.add('/img/icons/apple-icon-76x76.png')
+        cache.add('/img/icons/apple-icon-114x114.png')
+        cache.add('/img/icons/apple-icon-120x120.png')
+        cache.add('/img/icons/apple-icon-144x144.png')
+        cache.add('/img/icons/apple-icon-152x152.png')
+        cache.add('/img/icons/apple-icon-180x180.png')
+        cache.add('/img/icons/apple-icon-precomposed.png')
+        cache.add('/img/icons/apple-icon.png')
+        cache.add('/img/icons/favicon-16x16.png')
+        cache.add('/img/icons/favicon-36x36.png')
+        cache.add('/img/icons/favicon-96x96.png')
+        cache.add('https://fonts.googleapis.com/css?family=Raleway')
+      }))
+})
 
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'images',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
-  })
-)
+self.addEventListener('fetch', e => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => {
+        if (response) return response 
+
+        return fetch(event.request)
+      }))
+})
