6696eaaa184328bc7e4f121e0d074d4be59d02a3
Nathan Hipps <nathanhipps@gmail.com>
2019-02-05 16:51:58 -0500

    trying workbox again

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,45 +1,16 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches
-      .open('static')
-      .then(cache => {
-        cache.add('/')
-        cache.add('/about')
-        cache.add('/img/icons/android-icon-36x36.png')
-        cache.add('/img/icons/android-icon-48x48.png')
-        cache.add('/img/icons/android-icon-72x72.png')
-        cache.add('/img/icons/android-icon-96x96.png')
-        cache.add('/img/icons/android-icon-144x144.png')
-        cache.add('/img/icons/android-icon-192x192.png')
-        cache.add('/img/icons/apple-icon-57x57.png')
-        cache.add('/img/icons/apple-icon-60x60.png')
-        cache.add('/img/icons/apple-icon-72x72.png')
-        cache.add('/img/icons/apple-icon-76x76.png')
-        cache.add('/img/icons/apple-icon-114x114.png')
-        cache.add('/img/icons/apple-icon-120x120.png')
-        cache.add('/img/icons/apple-icon-144x144.png')
-        cache.add('/img/icons/apple-icon-152x152.png')
-        cache.add('/img/icons/apple-icon-180x180.png')
-        cache.add('/img/icons/apple-icon-precomposed.png')
-        cache.add('/img/icons/apple-icon.png')
-        cache.add('/img/icons/favicon-16x16.png')
-        cache.add('/img/icons/favicon-36x36.png')
-        cache.add('/img/icons/favicon-96x96.png')
-        cache.add('https://fonts.googleapis.com/css?family=Raleway')
-      }))
-})
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches
-      .match(e.request)
-      .then(response => {
-        if (response) {
-          return response
-        } else {
-          return fetch(e.request)
-        }
+
+workbox.routing.registerRoute(
+  new RegExp('img\/icons\/.*\.(png|jpg)'),
+  workbox.strategies.cacheFirst({
+    // Use a custom cache name
+    cacheName: 'icons-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 20 images
+        maxEntries: 40,
+        maxAgeSeconds: 7 * 24 * 60 * 60 * 52,
       })
-      .catch(error => console.log(error))
-  )
-})
+    ],
+  })
+)
