3416875548465151573999c7fefd3be36bedbb77
Nathan Hipps <nathanhipps@gmail.com>
2019-02-07 12:01:01 -0600

    add service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,12 @@
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
 
+workbox.precaching.suppressWarnings()
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+self.addEventListener("message", msg => {
+  if (msg.data.action == 'skipWaiting') self.skipWaiting()
+})
 
 workbox.routing.registerRoute(
   /img\/icons\/.*\.(png|jpg)/,
